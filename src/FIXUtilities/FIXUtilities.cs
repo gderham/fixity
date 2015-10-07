2318cb37e7f6fa3945a62dca52cdbd81b271a829
Guy Derham <guy.derham@gmail.com>
2015-10-07 15:49:59 +0000

    Add basic FIX message create/parse functions and a couple of tests.

---
+++ src/FIXUtilities/FIXUtilities.cs
@@ -0,0 +1,78 @@
+﻿namespace Fixity.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Functions that deal with FIX protocol specifics.
+    /// </summary>
+    public static class FIXUtilities
+    {
+        public const char FixDelimiter = '\x01';
+        
+        //TODO: Check which chars FIX accepts
+        private static Regex _fixFieldPattern = new Regex("([A-Za-z0-9]{1,2})=([A-Za-z0-9_ \\.]+)");
+
+        public static string SetFIXDelimiter(string message)
+        {
+            return message.Replace('|', FixDelimiter);
+        }
+
+        /// <summary>
+        /// Converts a | delimited string message to use the FIX
+        /// SOH delimiter and appends the correct checksum (tag 10).
+        /// </summary>
+        /// <param name="message">
+        /// For example: 8=FIXT1.1|9=100|35=A|49=SomeClient|56=SomeFacility|34=1|
+        /// </param>
+        /// <returns>
+        /// For example: 8=FIXT1.1[SOH]9=100[SOH]35=A[SOH]49=SomeClient[SOH]56=SomeFacility[SOH]34=1[SOH]10=21[SOH]
+        /// </returns>
+        public static string CreateFixMessage(string message)
+        {
+            if (!message.EndsWith("|"))
+            {
+                throw new ArgumentException("Message must be be terminated by a | delimiter.",
+                    "message");
+            }
+
+            return AddChecksum(SetFIXDelimiter(message));
+        }
+
+        /// <summary>
+        /// Calculates and appends a checksum to a FIX message that doesn't
+        /// have a checksum, and must be terminated by a [SOH] delimiter.
+        /// </summary>
+        /// <param name="message">A FIX message without checksum. </param>
+        private static string AddChecksum(string message)
+        {
+            int checksum = Encoding.ASCII.GetBytes(message).Sum(b => b) % 256;
+            
+            return string.Format("{0}10={1}{2}", message, checksum, FixDelimiter);
+        }
+
+        /// <summary>
+        /// Parse MessageType=Text pairs from a (possibly partial) FIX message
+        /// into a dictionary. No validation is performed.
+        /// </summary>
+        /// <returns>A dictionary of the MessageType->Text pairs.</returns>
+        public static Dictionary<string,string> ParseFixMessage(string message)
+        {
+            var matches = _fixFieldPattern.Matches(message);
+
+            var dict = new Dictionary<string,string>();
+            foreach (Match match in matches)
+            {
+                string tag = match.Groups[1].Value;
+                string value = match.Groups[2].Value;
+                dict.Add(tag, value);
+            }
+
+            return dict;
+        }
+
+    }
+}
---
+++ tests/unittests/CoreTests/FIXUtilitiesTests.cs
@@ -0,0 +1,81 @@
+﻿namespace Fixity.CoreTests
+{
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    using Core;
+
+    [TestFixture]
+    public class FIXUtilitiesTests
+    {
+        // FIX message format: (delimited by x01)
+        //   8=FIXT1.1
+        //  BodyLength (message length in bytes following this field up to and including the delimiter preceding the Checksum tag)
+        //   9=100
+        //  MsgType
+        //   35=A
+        //  SenderCompID
+        //   49=Client name
+        //  TargetCompID
+        //   56=Server name
+        //  MsgSeqNum
+        //   34=1
+        //  ...
+        //  Checksum
+        //   10=21 (sum of bytes up to that preceding the checksum mod 256)
+
+        //TODO: Move to separate class - with raw/fix format in same object
+        private readonly string _logonString1WithoutChecksum;
+        private readonly string _logonMessage1;
+        private readonly string _logonMessage1WithoutChecksum;
+
+        public FIXUtilitiesTests()
+        {
+            _logonString1WithoutChecksum = "8=FIXT1.1|9=100|35=A|49=SomeClient|56=SomeFacility|34=1|";
+
+            _logonMessage1 =
+                FIXUtilities.SetFIXDelimiter("8=FIXT1.1|9=100|35=A|49=SomeClient|56=SomeFacility|34=1|10=22|");
+            _logonMessage1WithoutChecksum =
+                FIXUtilities.SetFIXDelimiter("8=FIXT1.1|9=100|35=A|49=SomeClient|56=SomeFacility|34=1|");
+        }
+
+        #region CreateFixMessage tests
+
+        //TODO: Add more tests
+
+        [Test]
+        public void CreateFixMessage_AppendsCorrectChecksum_ForValidMessage()
+        {
+            string result = FIXUtilities.CreateFixMessage(_logonString1WithoutChecksum);
+
+            Assert.That(result, Is.EqualTo(_logonMessage1));
+        }
+
+        #endregion
+
+        #region ParseFixMessage tests
+
+        [Test]
+        public void ParseFixMessage_ReturnsCorrectFields_ForLogonMessage()
+        {   
+            var expected = new Dictionary<string, string>()
+            {
+                {"8","FIXT1.1"},
+                {"9","100"},
+                {"35","A"},
+                {"49","SomeClient"},
+                {"56","SomeFacility"},
+                {"34","1"},
+                {"10","22"}
+            };
+
+            Dictionary<string, string> result = FIXUtilities.ParseFixMessage(_logonMessage1);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        #endregion
+
+    }
+}
