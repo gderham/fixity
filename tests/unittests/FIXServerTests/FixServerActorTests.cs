121871da40f58b6db05fd433ef6f6c0ab662f703
Guy Derham <guy.derham@gmail.com>
2015-10-12 22:16:50 +0000

    Add first FIXServer unit test.

---
+++ tests/unittests/FIXServerTests/FixServerActorTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Fixity.Tests.FIXServerTests
+{
+    using System;
+
+    using Akka.Actor;
+    using Akka.TestKit.NUnit;
+    using NUnit.Framework;
+
+    using FIXServer.Actors;
+    using Core.Actors;
+
+    [TestFixture]
+    public class FixServerActorTests : TestKit
+    {
+        [Test]
+        public void StartListening_TellsTcpServerToStartListening_WhenReady()
+        {
+            var tcpServerActor = CreateTestProbe("TcpServer");
+            Func<IActorRefFactory, IActorRef> tcpServerCreator = (_) => tcpServerActor;
+            
+            var fixInterpreterActor = CreateTestProbe("FixInterpreter");
+            Func<IActorRefFactory, IActorRef> fixInterpreterCreator = (_) => fixInterpreterActor;
+
+            var fixServerProps = Props.Create(() => new FixServerActor(tcpServerCreator, fixInterpreterCreator));
+            var fixServerActor = ActorOf(fixServerProps);
+
+            fixServerActor.Tell(new FixServerActor.StartListening());
+
+            tcpServerActor.IgnoreMessages((message) => message is TcpServerActor.Subscribe);
+            tcpServerActor.ExpectMsg<TcpServerActor.StartListening>();
+        }
+
+
+    }
+}
